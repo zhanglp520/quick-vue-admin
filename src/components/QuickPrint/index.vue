c5f9ec8ed1e786b0222e15f5ca19c7d4509a68e9
土豆哥 <zhanglp15229380174@163.com>
2023-01-09 12:57:53 +0800

    增加按钮权限

---
+++ src/components/QuickPrint/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { toRefs } from 'vue'
+import { ArrowRight } from '@element-plus/icons-vue'
+
+const props = defineProps({
+  data: {
+    type: Array,
+    default: () => [],
+  },
+})
+const { data } = toRefs(props)
+</script>
+<template>
+  <div></div>
+</template>
